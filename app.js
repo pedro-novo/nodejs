2272a327318837178cbb07da309422b93f595e26
Pedro Teixeira <pedro.novo.93@gmail.com>
2022-02-20 16:57:56 +0000

    ejs

--- app.js
+++ app.js
@@ -3,23 +3,25 @@ const express = require("express");
 // Express app
 const app = express();
 
+// Register view engine
+app.set("view engine", "ejs");
+
 // List for requests
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about");
 });
 
-// Redirect
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create");
 });
 
 // 404 Page
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404");
 });
